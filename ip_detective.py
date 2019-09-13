30cdad690f21728f396fc14e54741e088208d145
Jost Luebbe <jostluebbe@utexas.edu>
2019-09-12 22:01:41 -0500

    tiny update

--- ip_detective.py
+++ ip_detective.py
@@ -39,6 +39,7 @@ def multithread_approach():
                 else:
                     e[ip_in] = ip_info
             except Exception as exception:
+
                 if 'Rate' in exception:
                     sleep(10)
                 logger.error(f'Unable to lookup IP {ip} because of error: {exception}')
